402ea73c0f8635292ee0e7f2226b6449d3d4b383
nikbritoff <65825157+nikbritoff@users.noreply.github.com>
2021-02-16 19:49:11 +0300

    Подгонка главной страницы

--- js/catalog.js
+++ js/catalog.js
@@ -1,16 +1,35 @@
 // Show max price from range
 
-let priceRange = document.querySelector('.price-range');
-let priceMaxRange = document.querySelector('.max-price');
+// let priceRange = document.querySelector('.price-range');
+// let priceMaxRange = document.querySelector('.max-price');
 
-priceRange.addEventListener('change', correctInputMaxPrice);
-priceMaxRange.addEventListener('change', correctRangeMaxPrice);
+// priceRange.addEventListener('change', correctInputMaxPrice);
+// priceMaxRange.addEventListener('change', correctRangeMaxPrice);
 
-function correctInputMaxPrice() {
-    priceMaxRange.value = priceRange.value;
+// function correctInputMaxPrice() {
+//     priceMaxRange.value = priceRange.value;
+// }
+
+// function correctRangeMaxPrice() {
+//     priceRange.value = priceMaxRange.value;
+// }
+
+
+
+// Popup
+const modal = document.querySelector('.modal-wrapper');
+const modalButtonClose = document.querySelector('.modal-close-button');
+const contactsButton = document.querySelector('.contacts-button');
+
+modalButtonClose.addEventListener('click', closeModal);
+contactsButton.addEventListener('click', openModal)
+
+function closeModal() {
+  modal.classList.add('display-none');
+  modal.classList.remove('modal-visible');
 }
 
-function correctRangeMaxPrice() {
-    priceRange.value = priceMaxRange.value;
+function openModal() {
+  modal.classList.add('modal-visible');
+  modal.classList.remove('display-none');
 }
-
